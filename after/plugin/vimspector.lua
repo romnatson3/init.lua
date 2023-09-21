61057c072a272d29ddb72b5442929f18b823f1ab
Roman <roman@gmail.com>
2023-09-21 19:07:23 +0300

    Add .vimspector.json

--- after/plugin/vimspector.lua
+++ after/plugin/vimspector.lua
@@ -7,3 +7,49 @@ vim.keymap.set('n', '<leader><F12>', ':call vimspector#Reset()<CR>', {silent = t
 vim.keymap.set('n', '<leader><F1>', ':call vimspector#ClearBreakpoints()<CR>', {silent = true})
 vim.keymap.set('n', '<leader><F5>', ':call vimspector#Launch()<CR>', {silent = true})
 vim.keymap.set('n', '<leader><F6>', ':call vimspector#Continue()<CR>', {silent = true})
+
+-- .vimspector.json, copy this file to project folder
+-- {
+--     "configurations": {
+--         "debugpy": {
+--             "adapter": "debugpy",
+--             "configuration": {
+--                 "type": "python",
+--                 "request": "launch",
+--                 "program": "${file}"
+--             },
+--             "breakpoints": {
+--                 "exception": {
+--                     "raised": "N",
+--                     "uncaught": "Y",
+--                     "userUnhandled": "N"
+--                 }
+--             }
+--         }
+--     }
+-- }
+--
+-- Django project config
+-- {
+--     "$shema": "https://puremourning.github.com/vimspector/schema/vimspector.schema.json",
+--     "configurations": {
+--         "django": {
+--             "adapter": "debugpy",
+--             "configuration": {
+--                 "request": "launch",
+--                 "python": "${VIRTUAL_ENV}/bin/python3",
+--                 "program": "${workspaceRoot}/manage.py",
+--                 "args": [ "runserver", "0.0.0.0:8000", "--noreload" ],
+--                 "django": true,
+--                 "stopOnEntry": true
+--             },
+--             "breakpoints": {
+--                 "exception": {
+--                     "raised": "N",
+--                     "uncaught": "Y",
+--                     "userUnhandled": "N"
+--                 }
+--             }
+--         }
+--     }
+-- }
