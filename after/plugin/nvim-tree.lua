6e38153362ee75aa4888a87cd8c39f70a7731bc1
Roman <roman@gmail.com>
2024-11-29 19:59:21 +0200

    Add bufferline, remove all not using plugins

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,40 +1,41 @@
--- -- disable netrw at the very start of your init.lua
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- -- set termguicolors to enable highlight groups
--- vim.opt.termguicolors = true
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
 
--- local function on_attach(bufnr)
---     local api = require "nvim-tree.api"
+vim.keymap.set('n', '<F5>', '<cmd>NvimTreeToggle<CR>')
 
---     local function opts(desc)
---         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---     end
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
---     -- default mappings
---     api.config.mappings.default_on_attach(bufnr)
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
---     -- custom mappings
---     -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
---     -- vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
--- end
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
 
--- require("nvim-tree").setup({
---     sort_by = "case_sensitive",
---     view = {
---         width = 30,
---     },
---     renderer = {
---         group_empty = true,
---     },
---     filters = {
---         dotfiles = false,
---     },
---     git = {
---         enable = true,
---         ignore = false,
---         timeout = 500,
---     },
---     on_attach = on_attach,
--- })
+  -- custom mappings
+  vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+  vim.keymap.set("n", ".", api.tree.change_root_to_node, opts("Toggle root"))
+end
+
+
+require("nvim-tree").setup({
+  on_attach = my_on_attach,
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -5,125 +5,117 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use({'wbthomason/packer.nvim'})
+    use {'wbthomason/packer.nvim'}
 
-    use({
+    use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.3',
         requires = { {'nvim-lua/plenary.nvim'} }
-    })
+    } 
 
     -- colorscheme
-    use({'sainnhe/sonokai'})
-    use({'ellisonleao/gruvbox.nvim'})
-    use({
+    use {'sainnhe/sonokai'}
+    use {'ellisonleao/gruvbox.nvim'}
+    use {
         'catppuccin/nvim',
         as = 'catppuccin'
-    })
-    use({'rebelot/kanagawa.nvim'})
-    use({'folke/tokyonight.nvim'})
-    use({
+    }
+    use {'rebelot/kanagawa.nvim'}
+    use {'folke/tokyonight.nvim'}
+    use {
         'rose-pine/neovim',
         as = 'rose-pine'
-    })
+    }
 
-    use({
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
-    })
-    use({'nvim-treesitter/playground'})
+    }
+    use {'nvim-treesitter/playground'}
 
-    use({'ThePrimeagen/harpoon'})
-    use({'mbbill/undotree'})
-    use({'tpope/vim-fugitive'})
-    use({'tpope/vim-commentary'})
+    use {'ThePrimeagen/harpoon'}
+    use {'mbbill/undotree'}
+    use {'tpope/vim-commentary'}
 
-    use({
+    use {'NeogitOrg/neogit',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'sindrets/diffview.nvim',
+            'nvim-telescope/telescope.nvim'
+        }
+    }
+
+    use {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
         'neovim/nvim-lspconfig'
-    })
+    }
 
-    use({
+    use {
         'hrsh7th/nvim-cmp',
         'hrsh7th/cmp-nvim-lsp',
         'L3MON4D3/LuaSnip',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path'
-    })
+    }
 
-    -- use({
-    --     'nvim-tree/nvim-tree.lua',
-    --     requires = { {'nvim-tree/nvim-web-devicons'} }
-    -- })
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
 
-    use({
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = 'v3.x',
-        requires = { 
-            'nvim-lua/plenary.nvim',
-            'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-            'MunifTanjim/nui.nvim',
-        }
-    })
-
-    use({
+    use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    })
+    }
 
-    use({
-        'romgrk/barbar.nvim',
-        requires = {
-            {'nvim-tree/nvim-web-devicons'},
-            {'lewis6991/gitsigns.nvim' },
-        }
-    })
-    
-    -- use({'nanozuki/tabby.nvim'})
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "*",
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
 
-    -- use({'vim-scripts/AutoComplPop'})
+    use {'lukas-reineke/indent-blankline.nvim'}
 
-    use({'lukas-reineke/indent-blankline.nvim'})
-
-    use({
+    use {
         'folke/trouble.nvim',
         requires = { 'nvim-tree/nvim-web-devicons' }
-    })
+    }
 
-    use({'github/copilot.vim'})
+    use {'github/copilot.vim'}
 
-    -- use({'puremourning/vimspector'})
-
-    use({'mfussenegger/nvim-dap'}) 
-    use({'mfussenegger/nvim-dap-python'})
-    use({'Weissle/persistent-breakpoints.nvim'})
-    use({
+    use {'mfussenegger/nvim-dap'}
+    use {'mfussenegger/nvim-dap-python'}
+    use {'Weissle/persistent-breakpoints.nvim'}
+    use {
         'rcarriga/nvim-dap-ui',
         requires = {'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio'} 
-    })
+    }
     
-    use({'akinsho/toggleterm.nvim', tag = '*',
+    use {'akinsho/toggleterm.nvim', tag = '*',
          config = function()
             require('toggleterm').setup()
         end
-    })
+    }
 
-    use({'norcalli/nvim-colorizer.lua',
+    use {'norcalli/nvim-colorizer.lua',
          config = function()
             require('colorizer').setup()
         end
-    })
+    }
 
-    use({'preservim/tagbar',
+    use {'preservim/tagbar',
         config = function()
             vim.keymap.set('n', '<F7>', ':TagbarToggle<CR>')
         end
-    })
+    }
 
-    use({'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'})
+    use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
 
-    use({'chentoast/marks.nvim'})
+    use {'chentoast/marks.nvim'}
 
-    use({'kien/ctrlp.vim'})
+    use {'kien/ctrlp.vim'}
+ 
 end)
